3e8214af18baf58d5c6c70bc701272fa853eb4f8
Mladen Turk <mturk@apache.org>
2021-11-12 17:54:50 +0100

    Fix mingw's auto inplace path conversion

--- runtest.sh
+++ runtest.sh
@@ -83,17 +83,17 @@ then
   export FOO="/tmp/foo/bar::unknown:/usr/local"
   rv="`$_cygwrun -e FOO=`"
   test ".$rv" = ".FOO=$FOO" || xbexit 1 "Failed #7.1: \`$rv'"
+  rv="`$_cygwrun -p -I=/tmp/foo`"
+  test ".$rv" = ".-I=$tmpdir\\foo" || xbexit 1 "Failed #7.2: \`$rv'"
 fi
 
 export FOO="/tmp/foo/bar:/usr/local::/usr/cc"
 rv="`$_cygwrun -e FOO=`"
 test ".$rv" = ".FOO=$tmpdir\\foo\\bar;$usrdir\\local;$usrdir\\cc" || xbexit 1 "Failed #8: \`$rv'"
 
-rv="`$_cygwrun -p -I=/tmp/foo`"
-test ".$rv" = ".-I=$tmpdir\\foo" || xbexit 1 "Failed #9: \`$rv'"
 
 rv="`$_cygwrun -p -I:./tmp/foo`"
-test ".$rv" = ".-I:.\\tmp\\foo" || xbexit 1 "Failed #10: \`$rv'"
+test ".$rv" = ".-I:.\\tmp\\foo" || xbexit 1 "Failed #9: \`$rv'"
 
 echo "All tests passed!"
 exit 0
