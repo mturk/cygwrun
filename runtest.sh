7fec3ff0efaa3755020dfde8237b29b05e76f1e1
Mladen Turk <mturk@apache.org>
2021-11-12 16:25:50 +0100

    Add more test cases

--- runtest.sh
+++ runtest.sh
@@ -75,5 +75,11 @@ export FOO="/tmp/foo/bar:/usr/local"
 rv="`$_cygwrun -e FOO=`"
 test ".$rv" = ".FOO=$tmpdir\\foo\\bar;$usrdir\\local" || xbexit 1 "Failed #6: \`$rv'"
 
+rv="`$_cygwrun -p -I=/tmp/foo`"
+test ".$rv" = ".-I=$tmpdir\\foo" || xbexit 1 "Failed #7: \`$rv'"
+
+rv="`$_cygwrun -p -I:./tmp/foo`"
+test ".$rv" = ".-I:.\\tmp\\foo" || xbexit 1 "Failed #8: \`$rv'"
+
 echo "All tests passed!"
 exit 0
