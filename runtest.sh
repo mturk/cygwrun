0c769eca163d71419050ab93997ccf32f66803aa
Mladen Turk <mturk@apache.org>
2021-11-12 19:00:31 +0100

    Fix faulty POSIX_ROOT test for mingw

--- runtest.sh
+++ runtest.sh
@@ -55,8 +55,10 @@ rm -rf $srcdir/.test 2>/dev/null
 test $rc -ne 0 && xbexit 1 "Failed #0"
 
 export POSIX_ROOT="C:\\_not\\a\\directory"
-rv="`$_cygwrun -p /tmp`"
-test ".$rv" = ".C:\\_not\\a\\directory\\tmp" || xbexit 1 "Failed #1: \`$rv'"
+v=/tmp
+test $phost = mingw && v=/$v
+rv="`$_cygwrun -p $v`"
+test ".$rv" = ".$POSIX_ROOT\\tmp" || xbexit 1 "Failed #1: \`$rv'"
 export POSIX_ROOT=
 
 export FOO=bar
