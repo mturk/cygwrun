322953d1518c848fa662389f4476f14c71d2da32
Mladen Turk <mturk@apache.org>
2021-11-17 21:22:15 +0100

    Bump to new 1.0.7-dev version

--- cygwrun.h
+++ cygwrun.h
@@ -29,7 +29,7 @@
  */
 #define PROJECT_MAJOR_VERSION   1
 #define PROJECT_MINOR_VERSION   0
-#define PROJECT_PATCH_VERSION   6
+#define PROJECT_PATCH_VERSION   7
 #if defined(_VENDOR_NUM)
 # define PROJECT_MICRO_VERSION  _VENDOR_NUM
 #else
@@ -51,7 +51,7 @@
 /**
  * Set to zero for non dev versions
  */
-#define PROJECT_ISDEV_VERSION   0
+#define PROJECT_ISDEV_VERSION   1
 
 #if PROJECT_ISDEV_VERSION
 # define PROJECT_VERSION_SFX    PROJECT_VENDOR_SFX "-dev"
