5a2c39ca0e07421983e2aabfc7f196fc35b8e243
Mladen Turk <mturk@apache.org>
2022-06-23 02:14:34 +0200

    Update release script

--- mkrelease.bat
+++ mkrelease.bat
@@ -17,30 +17,77 @@ rem
 rem --------------------------------------------------
 rem Cygwrun release helper script
 rem
-rem Usage: mkrelease.bat [version]
+rem Usage: mkrelease.bat version [options]
+rem    eg: mkrelease 1.1.2_1 "_VENDOR_SFX=_1"
 rem
 setlocal
-if not "x%~1" == "x" goto haveVersion
+if "x%~1" == "x" goto Einval
 rem
-echo Error: Missing release version
-echo Usage: %~nx0 [version]
-exit /b 1
-
-:haveVersion
-set "CygwrunVer=%~1"
+set "ProjectName=cygwrun"
+set "ReleaseArch=x64"
+set "ReleaseVersion=%~1"
+set "MakefileFlags=_STATIC_MSVCRT=1"
+shift
+:setArgs
+if "x%~1" == "x" goto doneArgs
+set "MakefileFlags=%MakefileFlags% %~1"
+shift
+goto setArgs
 rem
-set "ReleaseName=cygwrun-%CygwrunVer%-win-x64"
+:doneArgs
 rem
-freshclam.exe --quiet
-pushd x64
-echo ## Binary release v%CygwrunVer% > %ReleaseName%.txt
-echo. >> %ReleaseName%.txt
-echo. >> %ReleaseName%.txt
-echo ```no-highlight >> %ReleaseName%.txt
-clamscan.exe --version >> %ReleaseName%.txt
-clamscan.exe --bytecode=no cygwrun.exe >> %ReleaseName%.txt
-echo ``` >> %ReleaseName%.txt
-7za.exe a -bd %ReleaseName%.zip cygwrun.exe
-sigtool.exe --sha256 %ReleaseName%.zip >> %ReleaseName%-sha256.txt
+set "ReleaseName=%ProjectName%-%ReleaseVersion%-win-%ReleaseArch%"
+set "ReleaseLog=%ReleaseName%.txt
+pushd %~dp0
+set "BuildDir=%cd%"
 popd
 rem
+rem Create builds
+nmake /nologo /A %MakefileFlags%
+if not %ERRORLEVEL% == 0 goto Failed
+rem
+pushd "%ReleaseArch%"
+rem
+rem Get nmake and cl versions
+rem
+echo _MSC_FULL_VER > %ProjectName%.i
+nmake /? 2>%ProjectName%.p 1>NUL
+cl.exe /EP %ProjectName%.i >>%ProjectName%.p 2>&1
+rem
+echo ## Binary release v%ReleaseVersion% > %ReleaseLog%
+echo. >> %ReleaseLog%
+echo ```no-highlight >> %ReleaseLog%
+echo Compiled using: >> %ReleaseLog%
+echo nmake %MakefileFlags% >> %ReleaseLog%
+findstr /B /C:"Microsoft (R) " %ProjectName%.p >> %ReleaseLog%
+echo. >> %ReleaseLog%
+rem
+del /F /Q %ProjectName%.i 2>NUL
+del /F /Q %ProjectName%.p 2>NUL
+rem
+rem Set path for ClamAV and 7za if needed
+rem
+rem set "PATH=C:\Tools\clamav;C:\Utils;%PATH%"
+rem
+freshclam.exe --quiet
+clamscan.exe --version >> %ReleaseLog%
+clamscan.exe --bytecode=no %ProjectName%.exe >> %ReleaseLog%
+echo ``` >> %ReleaseLog%
+del /F /Q %ReleaseName%.zip 2>NUL
+7za.exe a -bd %ReleaseName%.zip %ProjectName%.exe
+sigtool.exe --sha256 %ReleaseName%.zip > %ReleaseName%-sha256.txt
+popd
+goto End
+rem
+:Einval
+echo Error: Invalid parameter
+echo Usage: %~nx0 version [options]
+exit /b 1
+
+:Failed
+echo.
+echo Error: Cannot build %ProjectName%.exe
+exit /b 1
+
+:End
+exit /b 0
