7b117ca37b984a518513cfcdacdf87ad06e7df42
Mladen Turk <mturk@apache.org>
2023-05-29 20:04:05 +0200

    Bump version to 1.2.1-dev after release

--- CHANGES.md
+++ CHANGES.md
@@ -6,6 +6,11 @@ For a full list of changes, see the [git commit log][log]
   [log]: https://github.com/mturk/cygwrun/commits/
 
 
+## v1.2.1
+
+ * In development
+
+
 ## v1.2.0
 
  * Fix searching for programs having relative paths
--- cygwrun.h
+++ cygwrun.h
@@ -33,7 +33,7 @@
  */
 #define PROJECT_MAJOR_VERSION   1
 #define PROJECT_MINOR_VERSION   2
-#define PROJECT_PATCH_VERSION   0
+#define PROJECT_PATCH_VERSION   1
 #if defined(_BUILD_NUMBER)
 # define PROJECT_MICRO_VERSION  _BUILD_NUMBER
 #else
@@ -42,7 +42,7 @@
 /**
  * Set to zero for non dev versions
  */
-#define PROJECT_ISDEV_VERSION   0
+#define PROJECT_ISDEV_VERSION   1
 
 /**
  * Helper macros for properly quoting a value as a
